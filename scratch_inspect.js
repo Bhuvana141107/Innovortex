f651b9eead0e2b7caa3edc988593b05729a3e0ed
Bhuvana141107 <bhuvanaranidatti@gmail.com>
2026-09-10 00:16:17 +0530

    Delete scratch_inspect.js

--- scratch_inspect.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const code = fs.readFileSync('figma_mock_data.js', 'utf8');
-
-const nrIdx = code.indexOf('nr=[');
-console.log('Users slice:\n', code.slice(nrIdx, nrIdx + 4000));
